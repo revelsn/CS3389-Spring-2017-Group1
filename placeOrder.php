9c12786935b591dfafc41c7c98ba49e2f11267ea
SonOfAgamemnon <theirlosthearts@gmail.com>
2017-04-25 14:24:26 -0500

    work dang you

--- placeOrder.php
+++ placeOrder.php
@@ -17,7 +17,6 @@
 			$stmt->execute();
 			$result = $stmt->fetch();
 			$order_id = $result['order_id'];
-			echo $order_id;
 		}
 		catch(PDOException $e){
 			die($e);
@@ -25,20 +24,18 @@
 			//echo $order_id;
 			foreach($_SESSION['orderItems'] as $key=>$value)
 			{
-				//echo $value;
-				/*try{
-					$sql = "SELECT item_id from inventory where name = :name)";
-					$stmt = $conn->prepare($sql);//->execute();
-					$stmt->bindParam(':name', $value, PDO::PARAM_STR);
-					$stmt->execute();
-					$result = $stmt->fetch();
+				echo $value;
+				try{
+					$sql = "SELECT item_id from inventory where name = \"$value\"";
+					$result = $conn->query($sql);
+					//$result = $stmt->fetch();
 					$item_id = $result['item_id'];
 				}
 				catch(PDOException $e){
 					die($e);
 				}
 				echo "<div>".$item_id."</div>";
-				try{
+				/*try{
 					$sql = "INSERT into order_items (order_id, item_id, quantity) values($order_id, $item_id, ".$_SESSION['orderAmounts'][$key].")";
 					$conn->prepare($sql)->execute();
 				}
