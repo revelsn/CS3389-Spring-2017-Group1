f07935bf68bc8c18101c1c9454f718e0eb7d511f
zackpace <665151@faulkner.edu>
2017-04-25 14:11:40 -0500

    added time to cart

--- viewCart.php
+++ viewCart.php
@@ -6,14 +6,45 @@
 		<div class="container">
 			<div class="jumbotron"><h1>Hello!</h1></div>
 			
+			
+				<div class="col-sm-10">
+			<div class="col-sm-10">
+			Select Pickup Day
+				<div class="col-sm-12">
+					<select class="selectpicker" id="Day" name="Day">
+						<option value="1">Monday</option>
+						<option value="2">Tuesday</option>
+						<option value="3">Wednesday</option>
+						<option value="4">Thursday</option>
+						<option value="5">Friday</option>
+					</select>
+				</div>
+				</div>
+			<div class="col-sm-10">
+			Select Pickup Time
+				<div class="col-sm-12">
+			<select class="selectpicker" id="Time" name= "Time">
+				<option value="10">10:00-11:00</option>
+				<option value="11">11:00-12:00</option>
+				<option value="12">12:00-1:00</option>
+				<option value="1">1:00-2:00</option>
+				<option value="2">2:00-3:00</option>
+				<option value="3">3:00-4:00</option>
+				<option value="4">4:00-5:00</option>
+				<option value="5">5:00-6:00</option>
+			</select>
+			</div>
+			</div>
 			<div class="row">
 				<div class="col-sm-6">
 					Cart
 				</div>
+				</div>
+			<div class="form-group">
 				<div class="col-sm-6 text-right">
 					<a class='btn btn-primary' href='placeOrder.php'><span class="glyphicon glyphicon-plus"></span>Place Order</a>
 				</div>
-			</div>
+			
 			<div class="row">	
 				<div class="col-sm-12">
 					<!--<form class="form-horizontal" action="WorkerInventoryView.php" method="POST">
