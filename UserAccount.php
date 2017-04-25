e0751208c385d8913a02dc18c36ac47084144e4e
weirdlo <agreen9@stu.ctcd.edu>
2017-04-25 07:25:38 -0500

    Add files via upload
    The start or the user/employee/admin account pages with the search bar.

---
+++ UserAccount.php
@@ -0,0 +1,39 @@
+<html>
+	<header>
+		<title>Wiggly Piggly::Account Page</title>
+	</header>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<!-- -->
+	<body style="font-family:georgian;">
+		<nav class="navbar navbar-transparent">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">Wiggly Piggly</a>
+				</div>
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="file:///C:/MAMP/htdocs/Grupo%20Uno/Home1.html">Home</a></li>
+					<li><a href="file:///C:/MAMP/htdocs/Grupo%20Uno/About1.html">About Us</a></li>
+					<li><a href="file:///C:/MAMP/htdocs/Grupo%20Uno/ContactInfo.html">Contact Us</a></li>
+				</ul>
+				<form class="col-md-8">
+					<div style="display:table;" class="input-group">
+						<input type="text" autofocus="autofocus" class="form-control" placeholder="Search">
+						<div class="input-group-btn">
+							<button class="btn btn-default" type="submit">
+								<i class="glyphicon glyphicon-search"></i>
+							</button>
+						</div>
+					</div>
+				</form>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="file:///C:/MAMP/htdocs/Grupo%20Uno/Login.php"><span class="glyphicon glyphicon-log-in"></span>Sign Out</a></li>
+				</ul>
+			</div>
+		</nav>	
+	</body>
+</html>
